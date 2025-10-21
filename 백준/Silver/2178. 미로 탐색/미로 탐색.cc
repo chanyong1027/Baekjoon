98af2b382ac5ddc8972188e68a583adb00f8d135
chanyong <137212216+chanyong1027@users.noreply.github.com>
2025-10-21 22:52:28 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+const int max_n = 104;
+int a[max_n][max_n], visited[max_n][max_n]; 
+const int dy[] = {-1, 0, 1, 0}; 
+const int dx[] = {0, 1, 0, -1};
+int n,m,x,y;
+
+int main(){
+	cin >> n >> m;
+	for(int i=0;i<n;i++){
+		string row;
+		cin >> row;
+		for(int j=0;j<m;j++){
+			a[i][j] = row[j] - '0';
+		}
+	}
+	queue<pair<int,int>> q;
+	visited[0][0] = 1;
+	q.push({0,0});
+	while(q.size()){
+		tie(y,x) = q.front(); q.pop();
+		for(int i=0;i<4;i++){
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue;
+			if(visited[ny][nx]) continue;
+			visited[ny][nx] = visited[y][x] + 1;  
+			q.push({ny,nx});
+		}
+	}
+	
+	printf("%d\n", visited[n - 1][m - 1]);
+}
