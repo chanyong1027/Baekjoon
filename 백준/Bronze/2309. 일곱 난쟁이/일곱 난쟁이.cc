f79505ecbc04fc77009ace6ee3383005b275aa25
chanyong <137212216+chanyong1027@users.noreply.github.com>
2026-04-01 16:27:46 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 int a[9];
-
+vector<int> v;
+int sum;
+pair<int,int> ret;
+void solve(){
+	for(int i=0;i<9;i++){
+		for(int j=0;j<i;j++){
+			if(sum - a[i] - a[j] == 100){
+                ret = {i,j};
+                return;
+            }
+		}
+	}
+}
 int main(){
 	for(int i=0;i<9;i++){
 		cin >> a[i];
+		sum +=a[i];
 	}
-	sort(a,a+9);
-	do{
-		int sum=0;
-		for(int i=0;i<7;i++) sum+=a[i];
-		if(sum == 100) break;
-	}while(next_permutation(a,a+9));
-	for(int i=0;i<7;i++) cout << a[i] << '\n';
+	solve();
+	for(int i=0;i<9;i++){
+		if(ret.first == i || ret.second == i) continue;
+		v.push_back(a[i]);
+	}
+	sort(v.begin(), v.end());
+	for(int i : v) cout << i << '\n';
+	return 0;
 }
