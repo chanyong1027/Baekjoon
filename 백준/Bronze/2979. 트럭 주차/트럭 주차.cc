1f6664a2e39c55143f0c0a807a7575e9bf381c70
chanyong <137212216+chanyong1027@users.noreply.github.com>
2026-04-01 17:25:25 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a,b,c,d,e,ret;
+int cnt[101];
+int main(){
+	cin >> a >> b >> c;
+	for(int i=0;i<3;i++){
+		cin >> d >> e;
+		for(int j=d;j<e;j++) cnt[j]++;
+	}
+	for(int i=1;i<=100;i++){
+		if(cnt[i] == 1) ret += a;
+		else if(cnt[i] == 2) ret += b * 2;
+		else if(cnt[i] == 3) ret += c * 3; 
+	}
+	cout << ret;
+}
