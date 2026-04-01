b62ea7155fc2765ec34504c0de4612c8dbc36355
chanyong <137212216+chanyong1027@users.noreply.github.com>
2026-04-01 18:38:53 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,24 +1,30 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
+string s,file;
 int n;
-string s,p,pre,suf;
 
-int main(){  
- ios_base::sync_with_stdio(false);
- cin >> n;
- cin >> p;
- pre = p.substr(0,p.find("*"));
- suf = p.substr(p.find("*")+1);
- 
- for(int i=0; i<n; i++){
- 	cin >> s;
- 	if(pre.size() + suf.size() > s.size()){
- 		cout << "NE\n";
-	 }
-	 else{
-	 	if(pre == s.substr(0,pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA\n";
-	 	else cout <<"NE\n";
-	 }
- }
- return 0;
+vector<string> split(const string& input, string delemeter){
+	vector<string> result;
+	auto start = 0;
+	auto end = input.find(delemeter);
+	while(end != string::npos){
+		result.push_back(input.substr(start, end - start));
+		start = end + delemeter.size();
+		end = input.find(delemeter, start);
+	}
+	result.push_back(input.substr(start));
+	return result;
+}
+int main(){
+	cin >> n;
+	cin >> s;
+	vector<string> da = split(s,"*");
+	string pre = da[0];
+	string suf = da[1];
+	for(int i=0;i<n;i++){
+		cin >> file;
+		if(file.size() < pre.size() + suf.size()) cout << "NE" << '\n';
+		else if(file.substr(0,pre.size()) == pre && file.substr(file.size()-suf.size()) == suf) cout << "DA" << '\n';
+		else cout << "NE" << '\n';
+	}
 }
