b85aefe7cd362322b8260b47b2ae046020ffdc09
chanyong <137212216+chanyong1027@users.noreply.github.com>
2026-04-02 16:25:06 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,20 +1,15 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-int n,k;
-int max_sum = INT_MIN;
-int psum[100005], a[100005];
-
-int main(){  
- ios_base::sync_with_stdio(false);
- cin >> n >> k;
- for(int i=1;i<=n;i++){
- 	cin >> a[i];
- 	psum[i] = psum[i-1] + a[i];
- }
- for(int i=1;i<=n-k+1;i++){
- 	max_sum = max(max_sum, psum[k+i-1] - psum[i-1]);
- }
- cout << max_sum;
- 
- return 0;
+int n,k,temp,ret = -10000004;
+int psum[100004];
+int main(){
+	cin >> n >> k;
+	for(int i=1;i<=n;i++){
+		cin >> temp;
+		psum[i] = psum[i-1] + temp;
+	}
+	for(int i=k;i<=n;i++){
+		ret = max(ret, psum[i] - psum[i-k]);
+	}
+	cout << ret;
 }
